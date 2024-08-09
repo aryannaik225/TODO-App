fb9958cc4cd18c5f100a172e222339a4420f84d3
aryannaik225 <aryann2203@gmail.com>
2024-08-09 23:10:40 +0530

    chore: Add functionality to add new todo items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
 
 const Todo = () => {
+
+const [todoList, setTodoList] = useState([]);
+
+const inputRef = useRef();
+
+const add = () => {
+  const inputText = inputRef.current.value.trim();
+  const newTodo = {
+    id: Date.now(),
+    text: inputText,
+    isComplete: false,
+  }
+}
+
   return (
     <div className='w-full h-screen flex justify-center items-center'>
         <div className='bg-white w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+
+          {/* -------- title -------- */}
+          <div className='flex items-center mt-7 gap-2'>
+            <img className='w-8' src={todo_icon} alt="" />
+            <h1 className='text-3xl font-semibold'>To-Do List</h1>
+          </div>
+
+          {/* -------- input field -------- */}
+          <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+            <input ref={inputRef} type="text" placeholder='Add your task' className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'/>
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+          </div>
+
+          {/* -------- todo list -------- */}
+          <div>
+            <TodoItems text="Learning Backend"/>
+          </div>
 
         </div>
     </div>
