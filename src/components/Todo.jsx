126a574ccbb28aa9ade24b9a2da4fecab28084cb
aryannaik225 <aryann2203@gmail.com>
2024-08-09 22:44:08 +0530

    chore: Add Todo component and update App layout

---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+        <div className='bg-white w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Todo
